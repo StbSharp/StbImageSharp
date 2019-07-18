f0efa2dd2ccf367aa8feac1869763bfcfb46a63f
Roman Shapiro <eleroman@mail.ru>
2019-07-18 23:06:31 +0700

    Updated build script

--- build_all.bat
+++ build_all.bat
@@ -1,5 +1,2 @@
 dotnet --version
-dotnet build build\StbImageSharp.sln /p:Configuration=Release --no-incremental
-call copy_zip_package_files.bat
-rename "ZipPackage" "StbImageSharp.%APPVEYOR_BUILD_VERSION%"
-7z a StbImageSharp.%APPVEYOR_BUILD_VERSION%.zip StbImageSharp.%APPVEYOR_BUILD_VERSION%
+dotnet build src\StbImageSharp\StbImageSharp.csproj /p:Configuration=Release --no-incremental
